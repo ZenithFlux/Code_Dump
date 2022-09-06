838a530d2d6b0888ef07bdebc2331b3f65b6c4b4
ZenithFlux <90706514+ZenithFlux@users.noreply.github.com>
2022-09-06 10:57:53 +0530

    clean_pc.py added

---
+++ Python/clean_pc.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+APPDATA = os.getenv('appdata')
+LOCALAPPDATA = os.getenv('localappdata')
+PROGRAMDATA = os.getenv('programdata')
+
+paths = [
+    f"{PROGRAMDATA}\\NVIDIA Corporation\\Downloader",
+    f"{LOCALAPPDATA}\\CrashDumps",
+    f"{LOCALAPPDATA}\\Temp",
+    f"{LOCALAPPDATA}\\pip\\cache",
+    f"{LOCALAPPDATA}\\npm-cache",
+    f"{LOCALAPPDATA}Low\\Temp",
+    f"{APPDATA}\\discord\\Cache",
+    f"{APPDATA}\\discord\\Code Cache",
+    f"{APPDATA}\\discord\\GPUCache",
+    f"{APPDATA}\\Code\\Cache",
+    f"{APPDATA}\\Code\\CachedData",
+    f"{APPDATA}\\Code\\CachedExtensions",
+    f"{APPDATA}\\Code\\Code Cache"
+]
+
+paths.sort()
+
+print("Deleting...")
+for path in paths:
+    if os.path.exists(path):
+        print(path)
+    shutil.rmtree(path, ignore_errors=True)
+    
+input("\nPC cleaned...")
